922da0c6cd370e81f3d98f70504144c48cf6e9f0
juney <juhnnie@gmail.com>
2019-06-11 18:58:04 -0700

    i dont know

--- Cook Book App/js/init.js
+++ Cook Book App/js/init.js
@@ -1,10 +1,3 @@
-(function($){
-  $(function(){
-
-    $('.sidenav').sidenav();
-    $('.parallax').parallax();
-
-  }); // end of document ready
-})(jQuery); // end of jQuery name space
-
-$(".dropdown-trigger").dropdown();
+$(document).ready(function(){
+  $('.sidenav').sidenav();
+});
